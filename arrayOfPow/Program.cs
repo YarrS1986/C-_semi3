2c83b12567eed9c8fc6ecdcbd5f3b258854f1030
Yaroslav.S <sysoev.yar@yandex.ru>
2022-03-31 19:39:43 +0500

    3я задача. массив квадратных чисел

---
+++ arrayOfPow/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// например,          5 -> 1, 4, 9, 16, 25.                2 -> 1,4
+
+Console.Clear();
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strMessage = Console.ReadLine();
+    int answer = int.Parse(strMessage);
+    return answer;
+}
+
+int array = Promt("Введи длину массива = ");
+int index = 1;
+
+while (index < array)
+{
+    Console.Write($"{index*index}, ");
+    index++;
+}
+Console.Write($"{index*index}.");
