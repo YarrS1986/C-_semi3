45d571309c42a4418d4b95df11b1a1c7299ca268
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-01 22:18:43 +0500

    дз3 таблица кубов от 1 до N

---
+++ HomeWork3_arrayOfKubes/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// например,          3 -> 1, 8, 27.                5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strMessage = Console.ReadLine();
+    int answer = int.Parse(strMessage);
+    return answer;
+}
+
+int array = Promt("Введи длину массива = ");
+int index = 1;
+
+while (index < array)
+{
+    Console.Write($"{index*index*index}, ");
+    index++;
+}
+Console.Write($"{index*index*index}.");
