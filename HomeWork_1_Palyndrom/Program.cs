18e0ed67af7b40560a70c607b3a8996af2c21619
Yaroslav.S <sysoev.yar@yandex.ru>
2022-04-05 20:11:31 +0500

    попытка номер 2 - полиндромы

--- HomeWork_1_Palyndrom/Program.cs
+++ HomeWork_1_Palyndrom/Program.cs
@@ -3,22 +3,54 @@
 
 Console.Clear();
 
-int Promt(string message)
-{
-    Console.Write(message);
-    string strMessage = Console.ReadLine();
-    int answer = int.Parse(strMessage);
-    return answer;
-}
-int value = Promt("Введи пятизначное число: ");
-valueLen = value.ToString().Length;
+// int Promt(string message)
+// {
+//     Console.Write(message);
+//     string strMessage = Console.ReadLine();
+//     int answer = int.Parse(strMessage);
+//     return answer;
+// }
 
-for (int i = 0; i < value.Lenght / 2; i++)
+Console.Write("введи число = ");
+string num = Console.ReadLine();
+int len = num.Length;
+
+int i = 0;
+while (i < len / 2)
 {
-    if (value[i] != value[value.Lenght - i - 1])
-        Console.WriteLine("нет");
+    if (num[i] == num[len - 1 - i])
+    {
+        Console.WriteLine("полиндром");
+        i++;
+    }
+    else
+    {
+        Console.WriteLine("не полиндром");
+        break;
+    }
 }
-Console.WriteLine("да");
+//for (int i = 0; i < len / 2; i++)
+
+
+
+
+// int value = int.Parse(strMessage);
+// int[] array = new int[int.Parse(strMessage)];
+
+// // int value = Promt("Введи пятизначное число: ");
+// // int num = value.Lenght;
+
+// 
+// {
+//     Console.WriteLine(array[i]);
+//     i++;
+// if (array[i] == array[Lenght - i])
+//     Console.WriteLine(array[i]);
+
+//Console.WriteLine("полиндром");
+
+// 0 1 2 3 4 5 6
+
 
 // if (value > 9999 && value < 100000)
 // {
